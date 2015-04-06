78bf8c47d83a013b33e8e0855b339b2e88301714
Chase Lambert <theicfire@gmail.com>
2015-04-06 10:50:23 -0700

    Removing one level of "describe"
    I thought two levels were needed but that's not true at all.
    Two or even more is fine, but also one is!

--- js/test/UndoRing.js
+++ js/test/UndoRing.js
@@ -2,95 +2,87 @@ var assert = require("assert");
 var _ = require("underscore");
 var UndoRing = require('../lib/UndoRing');
 
-describe('UndoRing', function(){
-    describe('Initialize', function(){
-        it('Length of buffer should be 10', function(){
-            var ring = new UndoRing(0, 10);
-            assert.equal(10, ring.length);
-        })
+describe('Initialize', function(){
+    it('Length of buffer should be 10', function(){
+        var ring = new UndoRing(0, 10);
+        assert.equal(10, ring.length);
     })
 })
 
-describe('UndoRing', function(){
-    describe('more', function(){
-        it('blah', function(){
-            var ring = new UndoRing(0, 4);
-            assert.equal(ring.ring[0], 0);
+describe('more', function(){
+    it('blah', function(){
+        var ring = new UndoRing(0, 4);
+        assert.equal(ring.ring[0], 0);
 
-            ring.ring = [0, 1, 2, 3];
-            assert(_.isEqual([0, 1, 2, 3], ring.ring));
-            assert.equal(0, ring.start);
-            assert.equal(0, ring.end);
+        ring.ring = [0, 1, 2, 3];
+        assert(_.isEqual([0, 1, 2, 3], ring.ring));
+        assert.equal(0, ring.start);
+        assert.equal(0, ring.end);
 
-            ring.addPending(5);
-            ring.commit();
-            assert(_.isEqual([0, 5, 2, 3], ring.ring));
-            assert.equal(1, ring.start);
-            assert.equal(0, ring.end);
+        ring.addPending(5);
+        ring.commit();
+        assert(_.isEqual([0, 5, 2, 3], ring.ring));
+        assert.equal(1, ring.start);
+        assert.equal(0, ring.end);
 
-            ring.addPending(6);
-            ring.commit();
-            ring.addPending(7);
-            ring.commit();
-            ring.addPending(8);
-            ring.commit();
-            assert(_.isEqual([8, 5, 6, 7], ring.ring));
-            assert.equal(0, ring.start);
-            assert.equal(1, ring.end);
-        })
+        ring.addPending(6);
+        ring.commit();
+        ring.addPending(7);
+        ring.commit();
+        ring.addPending(8);
+        ring.commit();
+        assert(_.isEqual([8, 5, 6, 7], ring.ring));
+        assert.equal(0, ring.start);
+        assert.equal(1, ring.end);
     })
 })
 
 
-describe('UndoRing', function(){
-    describe('undoredo', function(){
-        it('undoredo', function(){
-            var ring = new UndoRing(0, 4);
-            ring.addPending(5);
-            ring.commit();
-            ring.addPending(6);
-            ring.commit();
-            ring.addPending(7);
-            ring.commit();
-            ring.addPending(8);
-            ring.commit();
-            assert.equal(7, ring.undo());
-            assert.equal(6, ring.undo());
-            assert.equal(5, ring.undo());
-            assert.equal(5, ring.undo());
-            assert.equal(6, ring.redo());
+describe('undoredo', function(){
+    it('undoredo', function(){
+        var ring = new UndoRing(0, 4);
+        ring.addPending(5);
+        ring.commit();
+        ring.addPending(6);
+        ring.commit();
+        ring.addPending(7);
+        ring.commit();
+        ring.addPending(8);
+        ring.commit();
+        assert.equal(7, ring.undo());
+        assert.equal(6, ring.undo());
+        assert.equal(5, ring.undo());
+        assert.equal(5, ring.undo());
+        assert.equal(6, ring.redo());
 
-            ring.addPending(10);
-            ring.commit();
-            assert.equal(6, ring.undo());
-            assert.equal(5, ring.undo());
-            assert.equal(6, ring.redo());
-            assert.equal(10, ring.redo());
-            assert.equal(10, ring.redo());
+        ring.addPending(10);
+        ring.commit();
+        assert.equal(6, ring.undo());
+        assert.equal(5, ring.undo());
+        assert.equal(6, ring.redo());
+        assert.equal(10, ring.redo());
+        assert.equal(10, ring.redo());
 
-            ring.addPending(11);
-            ring.commit();
-            assert.equal(10, ring.undo());
-            assert.equal(6, ring.undo());
-            assert.equal(5, ring.undo());
-            assert.equal(5, ring.undo());
+        ring.addPending(11);
+        ring.commit();
+        assert.equal(10, ring.undo());
+        assert.equal(6, ring.undo());
+        assert.equal(5, ring.undo());
+        assert.equal(5, ring.undo());
 
-            ring.addPending(12);
-            ring.commit();
-            assert.equal(5, ring.undo());
-            assert.equal(5, ring.undo());
-        })
+        ring.addPending(12);
+        ring.commit();
+        assert.equal(5, ring.undo());
+        assert.equal(5, ring.undo());
     })
 })
 
-describe('UndoRing', function(){
-    describe('undoredo', function(){
-        it('undoredo', function(){
-            var ring = new UndoRing(0, 4);
-            ring.addPending(5);
-            ring.commit();
-            assert.equal(0, ring.undo());
-            assert.equal(0, ring.undo());
-        })
+describe('undoredo', function(){
+    it('undoredo', function(){
+        var ring = new UndoRing(0, 4);
+        ring.addPending(5);
+        ring.commit();
+        assert.equal(0, ring.undo());
+        assert.equal(0, ring.undo());
     })
 })
