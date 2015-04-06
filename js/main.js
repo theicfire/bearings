c05aaf2b9c0a70ba3bac359c60d2f281d93b7365
Chase Lambert <theicfire@gmail.com>
2015-04-06 10:37:52 -0700

    Renaming TreeNode to ReactTree

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var _ = require('underscore');
 var $ = require('jquery');
-var StartRender = require('./TreeNode');
+var StartRender = require('./ReactTree');
 var Tree = require('./lib/Tree');
 var Parse = require('parse').Parse;
 Parse.initialize("R7ngCzUNFlUFW2jZO90HfG7Pgr8Roa0dgIsFknNJ", "hvTrWozZrInn3qmEuE2zlscYBNRwUWjkndcIwIOL");
