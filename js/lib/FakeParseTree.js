67a201bae240f9af8f65a98170db58b527fa5664
Chase Lambert <chase@stripe.com>
2015-05-07 23:26:25 -0700

    Make parse configurable/ turn on/off
    Also rolled keys so I'm the only one that knows
    about the new ones

---
+++ js/lib/FakeParseTree.js
@@ -0,0 +1,10 @@
+var FakeParseTree = function(tree) {
+    this.tree = tree;
+};
+FakeParseTree.prototype.get = function(prop) {
+    console.assert(prop === 'tree');
+    return this.tree;
+};
+FakeParseTree.prototype.set = function() {}
+FakeParseTree.prototype.save = function() {}
+module.exports = exports = FakeParseTree;
--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@ var _ = require('underscore');
 var $ = require('jquery');
 var StartRender = require('./ReactTree');
 var Tree = require('./lib/Tree');
-var Parse = require('parse').Parse;
-Parse.initialize("R7ngCzUNFlUFW2jZO90HfG7Pgr8Roa0dgIsFknNJ", "hvTrWozZrInn3qmEuE2zlscYBNRwUWjkndcIwIOL");
+var config = require('./config');
+var FakeParseTree = require('./lib/FakeParseTree');
 
-
-var TestObject = Parse.Object.extend("TestObject");
-var query = new Parse.Query(TestObject);
-query.get("qJbAFzSI53", {
-    success: function (parseTree) {
-        StartRender(parseTree);
-    },
-    error: function(obj, error) {
-        throw('Error loading tree' + obj + error);
-    }
-});
-
+if (config.use_parse) {
+    var Parse = require('parse').Parse;
+    Parse.initialize(config.parse_app_id,config.parse_js_key);
+    var TestObject = Parse.Object.extend("first");
+    var query = new Parse.Query(TestObject);
+    query.get("emh8hVrhnC", {
+        success: function (parseTree) {
+            StartRender(parseTree);
+        },
+        error: function(obj, error) {
+            throw('Error loading tree' + obj + error);
+        }
+    });
+} else {
+    StartRender(new FakeParseTree(Tree.toString(Tree.makeDefaultTree())));
+}
